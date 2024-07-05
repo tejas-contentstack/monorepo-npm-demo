152dcf9f054de2ab8cdc0a93469b9f966da2ca79
tejas-contentstack <132910969+tejas-contentstack@users.noreply.github.com>
2024-07-05 23:16:23 +0530

    log response and block request for other domain

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -5,7 +5,8 @@ export default async function handler(request) {
         const response = await fetch(request)
         const newHeaders = new Headers(response.headers)
         newHeaders.set('X-Robots-Tag', 'noindex')
-
+        console.log("response")
+        console.log(response)
         return new Response(response.body, {
             status: response.status,
             statusText: response.statusText,
@@ -13,7 +14,7 @@ export default async function handler(request) {
         });
       }
     
-      if (hostname === 'monorepo-npm-demo.devcontentstackapps.com') {
+      if (hostname === 'domain-number-2.contentstackapps.com') {
         return new Response('Access to this site is blocked.', { status: 403 })
       }
     console.log('Edge log identifier: contentfly-management-background-jobs-service-dev-deploy:1ff7e98cc1b0-1b0f3dc9d82f:run-counter-1')
