095526f34d8d577e4248121e0ad340980884421a
tejas-contentstack <132910969+tejas-contentstack@users.noreply.github.com>
2024-07-05 21:26:54 +0530

    update edge function

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,4 +1,3 @@
-
 export default async function handler(request) {
     console.log('Edge log identifier: contentfly-management-background-jobs-service-dev-deploy:1ff7e98cc1b0-1b0f3dc9d82f:run-counter-1')
     return fetch(request);
