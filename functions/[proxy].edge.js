c655108e17d35165291dfaa6544ee49a775c29f3
tejas-contentstack <132910969+tejas-contentstack@users.noreply.github.com>
2024-07-05 21:18:11 +0530

    update custom edge function

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,6 +1,5 @@
-import jwt from '@tsndr/cloudflare-worker-jwt';
 
-export default async function handler(request, context) {
-    console.log("I'm from edge")
+export default async function handler(request) {
+    console.log('Edge log identifier: contentfly-management-background-jobs-service-dev-deploy:1ff7e98cc1b0-1b0f3dc9d82f:run-counter-1')
     return fetch(request);
 }
