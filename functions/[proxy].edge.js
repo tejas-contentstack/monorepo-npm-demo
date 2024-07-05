49aeb522e4a5b646b04bf1b686d56ef9ce6421ec
tejas-contentstack <132910969+tejas-contentstack@users.noreply.github.com>
2024-07-05 23:02:12 +0530

    Use the hostname from the request.url

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,4 +1,6 @@
 export default async function handler(request) {
+    const url = new URL(request.url)
+    const hostname = url.hostname
     if (hostname === 'monorepo-npm-demo.contentstackapps.com' ) {
         const response = await fetch(request)
         const newHeaders = new Headers(response.headers)
