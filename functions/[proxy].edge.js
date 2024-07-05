2ae950f3f8e3bc684de04e0c9c08e43d916ea282
tejas-contentstack <132910969+tejas-contentstack@users.noreply.github.com>
2024-07-05 22:48:31 +0530

    block the request from dev domain

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,5 +1,5 @@
 export default async function handler(request) {
-    if (hostname === 'monorepo-npm-demo.devcontentstackapps.com') {
+    if (hostname === 'monorepo-npm-demo.contentstackapps.com' ) {
         const response = await fetch(request)
         const newHeaders = new Headers(response.headers)
         newHeaders.set('X-Robots-Tag', 'noindex')
@@ -11,6 +11,9 @@ export default async function handler(request) {
         });
       }
     
+      if (hostname === 'monorepo-npm-demo.devcontentstackapps.com') {
+        return new Response('Access to this site is blocked.', { status: 403 })
+      }
     console.log('Edge log identifier: contentfly-management-background-jobs-service-dev-deploy:1ff7e98cc1b0-1b0f3dc9d82f:run-counter-1')
     return fetch(request);
 }
