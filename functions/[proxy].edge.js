1db46b4242ea719ddad696582eec3244d5cba771
Tejas Talekar <tejas.talekar@contentstack.com>
2024-07-07 00:11:44 +0530

    add try catch block

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -7,18 +7,34 @@ export default async function handler(request) {
        * Response and pass modified status or statusText in the ResponseInit
        * object. Response headers can be modified through the headers `set` method.
        */
-      const originalResponse = await fetch(request);
-      const responseHeaderEntries = originalResponse.headers.entries()
-      let responseHeaders = new Headers(responseHeaderEntries);
-      responseHeaders.set("X-Robots-Tag", "noindex");
+      // const originalResponse = await fetch(request);
+      // const responseHeaderEntries = originalResponse.headers.entries()
+      // let responseHeaders = new Headers(responseHeaderEntries);
+      // responseHeaders.set("X-Robots-Tag", "noindex");
       
-      const response = new Response(originalResponse.body, {
-        ...originalResponse,
-        headers: responseHeaders,
-        status: originalResponse.statusCode,
-        statusText: originalResponse.statusText,
-      });
-      console.log('reaching here without an error message')
+      // const response = new Response(originalResponse.body, {
+      //   ...originalResponse,
+      //   headers: responseHeaders,
+      //   status: originalResponse.statusCode,
+      //   statusText: originalResponse.statusText,
+      // });
+    let response;
+    try {
+      response = await fetch(request)
+  
+      // Clone the response so that it's no longer immutable
+      const newResponse = new Response(response.body, response)
+      
+      // Add a custom header with a value
+      newResponse.headers.append("X-Robots-Tag", "noindex")
+      
+    } catch (error) {
+      console.log('reaching here with an error message');
+      console.log(error);      
+    }
+      
+      
+      console.log('reaching here without an error message2')
       return response; // Explicitly return the modified response
     }
   
