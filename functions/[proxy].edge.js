d890983fba58efb98e9d872c1295dd6e0d7b8686
Tejas Talekar <tejas.talekar@contentstack.com>
2024-07-06 12:48:28 +0530

    Copy status, status text, headers from original response

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,25 +1,32 @@
 export default async function handler(request) {
-    const url = new URL(request.url)
-    const hostname = url.hostname
-    if (hostname === 'monorepo-npm-demo.contentstackapps.com' ) {
-        const response = await fetch(request)
-        
-        const newResponse = response.clone();
-        console.log("response")
-        console.log(response?.body)
-        console.log(response?.status)
-      // Add a custom header with a value
-      newResponse.headers.append(
-        "X-Robots-Tag",
-        "noindex"
-      );
-
-      return newResponse;
-      }
-    
-      if (hostname === 'domain-number-2.contentstackapps.com') {
-        return new Response('Access to this site is blocked.', { status: 403 })
-      }
-    console.log('Edge log identifier: contentfly-management-background-jobs-service-dev-deploy:1ff7e98cc1b0-1b0f3dc9d82f:run-counter-1')
-    return fetch(request);
+  const url = new URL(request.url)
+  const hostname = url.hostname
+  if (hostname === 'monorepo-npm-demo.contentstackapps.com' ) {
+      /**
+       * Response properties are immutable. To change them, construct a new
+       * Response and pass modified status or statusText in the ResponseInit
+       * object. Response headers can be modified through the headers `set` method.
+       */
+      const originalResponse = await fetch(request);
+  
+      let response = new Response(originalResponse.body, {
+        status: originalResponse.status,
+        statusText: originalResponse.statusText,
+        headers: originalResponse.headers,
+      });
+  
+      response = new Response(originalResponse.body, response);
+  
+      // Add a header using set method
+      response.headers.set("X-Robots-Tag", "noindex");
+  
+  
+      return response;
+    }
+  
+    if (hostname === 'domain-number-2.contentstackapps.com') {
+      return new Response('Access to this site is blocked.', { status: 403 })
+    }
+  console.log('Edge log identifier: contentfly-management-background-jobs-service-dev-deploy:1ff7e98cc1b0-1b0f3dc9d82f:run-counter-1')
+  return fetch(request);
 }
