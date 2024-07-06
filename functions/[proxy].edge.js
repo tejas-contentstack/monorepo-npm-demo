3e5fae41c4f7d3261d71b4cf80bc283404318f4a
Tejas Talekar <tejas.talekar@contentstack.com>
2024-07-07 00:20:23 +0530

    update the code

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,54 +1,29 @@
 export default async function handler(request) {
-  const url = new URL(request.url)
-  const hostname = url.hostname
-  if (hostname === 'monorepo-npm-demo.contentstackapps.com' ) {
-      /**
-       * Response properties are immutable. To change them, construct a new
-       * Response and pass modified status or statusText in the ResponseInit
-       * object. Response headers can be modified through the headers `set` method.
-       */
-      // const originalResponse = await fetch(request);
-      // const responseHeaderEntries = originalResponse.headers.entries()
-      // let responseHeaders = new Headers(responseHeaderEntries);
-      // responseHeaders.set("X-Robots-Tag", "noindex");
-      
-      // const response = new Response(originalResponse.body, {
-      //   ...originalResponse,
-      //   headers: responseHeaders,
-      //   status: originalResponse.statusCode,
-      //   statusText: originalResponse.statusText,
-      // });
-    let response;
-    return await fetch(request);
+  const url = new URL(request.url);
+  const hostname = url.hostname;
+
+  if (hostname === 'monorepo-npm-demo.contentstackapps.com') {
     try {
-      response = await fetch(request)
-  
-      // Clone the response so that it's no longer immutable
-      const newResponse = new Response(response.body, response)
-      
+      const response = await fetch(request);
+
+      // Clone the response for modification
+      const newResponse = new Response(response.body, response);
+
       // Add a custom header with a value
-      newResponse.headers.append("X-Robots-Tag", "noindex")
-      
+      newResponse.headers.append("X-Robots-Tag", "noindex");
+
+      return newResponse;
     } catch (error) {
-      console.log('reaching here with an error message');
-      console.log(error);      
+      console.log('Error fetching response:', error);
+      // Handle the error here (e.g., return a generic error response)
+      return new Response('An error occurred.', { status: 500 });
     }
-      
-      
-      console.log('reaching here without an error message2')
-      return response; // Explicitly return the modified response
-    }
-  
-    if (hostname === 'domain-number-2.contentstackapps.com') {
-      return new Response('Access to this site is blocked.', { status: 403 })
-    }
+  }
+
+  if (hostname === 'domain-number-2.contentstackapps.com') {
+    return new Response('Access to this site is blocked.', { status: 403 })
+  }
+
   console.log('Edge log identifier: contentfly-management-background-jobs-service-dev-deploy:1ff7e98cc1b0-1b0f3dc9d82f:run-counter-1')
   return fetch(request);
 }
-
-const request = new Request({
-  method: "GET",
-  url: "monorepo-npm-demo.contentstackapps.com"
-})
-
-const response = await handler(request);
