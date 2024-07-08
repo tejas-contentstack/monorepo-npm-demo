9280659a38ca3bf4e09b7c3b8020a5f67350e1d7
Tejas Talekar <tejas.talekar@contentstack.com>
2024-07-08 13:00:16 +0530

    comment altering header

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -10,7 +10,7 @@ export default async function handler(request, context) {
     })
     console.log("post modifiedResponse");
 
-    modifiedResponse.headers.append("X-Robots-Tag", "noindex");
+    // modifiedResponse.headers.append("X-Robots-Tag", "noindex");
     
     return modifiedResponse;
     // try {
